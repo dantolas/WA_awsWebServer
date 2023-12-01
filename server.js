3bf8eda1987e7541fb63cd0f361ed2d7d18bcb68
Prince Charming <kuta.samuel@gmail.com>
2023-12-01 10:20:50 +0100

    Favicon works, uncommenting scripts

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ server.get("/favicon.ico",(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/resources/favicon.ico'));
 })
 
-// server.use(express.static(path.join(__dirname,'/public/scripts')),
-// (req,res) => {
-//     res.set('content-type', 'application/javascript;charset=utf-8');
-// });
+server.use(express.static(path.join(__dirname,'/public/scripts')),
+(req,res) => {
+    res.set('content-type', 'application/javascript;charset=utf-8');
+});
 
 // // Base routes
 
